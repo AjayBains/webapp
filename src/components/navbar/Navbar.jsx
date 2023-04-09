9b96ad52e8ec062903ae6dc832eae72302b90120
Ajay_Bains <ajay.bains@codvo.ai>
2023-04-09 07:53:59 +0530

    buildin fe

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const { pathname } = useLocation();
+
+  const isActive = () => {
+    window.scrollY > 100 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "Ajay",
+    isSeller: true,
+  };
+  return (
+    <div className={active || pathname != "/" ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          <Link to="/" className="link">
+            <span className="text">gigshore</span>
+          </Link>
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>GigShore Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          {!currentUser.isSeller && <span>Become a seller</span>}
+          {!currentUser && <button>Sign up</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen((prev) => !prev)}>
+              <img
+                src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80"
+                alt=""
+              />
+              <span>{currentUser?.username}</span>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <Link to="/mygigs">Gigs</Link>
+                      <Link to="/add">Add New Gig</Link>
+                    </>
+                  )}
+                  <Link to="/orders">Orders</Link>
+                  <Link to="/messages">Messages</Link>
+                  <Link>Logout</Link>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      {(active || pathname != "/") && (
+        <>
+          <hr />
+          <div className="menu">
+            <Link className="link menuLink" to="/">
+              Graphics & Design
+            </Link>
+            <Link className="link menuLink" to="/">
+              Video & Animation
+            </Link>
+            <Link className="link menuLink" to="/">
+              Writing & Translation
+            </Link>
+            <Link className="link menuLink" to="/">
+              AI Services
+            </Link>
+            <Link className="link menuLink" to="/">
+              Digital Marketing
+            </Link>
+            <Link className="link menuLink" to="/">
+              Music & Audio
+            </Link>
+            <Link className="link menuLink" to="/">
+              Programming & Tech
+            </Link>
+            <Link className="link menuLink" to="/">
+              Business
+            </Link>
+            <Link className="link menuLink" to="/">
+              Lifestyle
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
